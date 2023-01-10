501ff8d7dcbd68d12431d0dc191ca0b4ff844d71
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-01-10 11:06:09 +0100

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,27 +1,61 @@
 # imports
 import os
 import csv
-import joblib
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
 
-# parse arguments
+import numpy as np
+import pandas as pd
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+
+sys.path.insert(0, './model/framework')
+
+from predictors.rlm.rlm_predictor import RLMPredictior
+from predictors.utilities.utilities import addMolsKekuleSmilesToFrame
+
+
+def predict_df(smiles_list, smi_column_name='smiles', models=['rlm']):
+    
+    df = pd.DataFrame({smi_column_name: smiles_list})
+    
+    response = {}
+    working_df = df.copy()
+    addMolsKekuleSmilesToFrame(working_df, smi_column_name)
+    working_df = working_df[~working_df['mols'].isnull() & ~working_df['kekule_smiles'].isnull()]
+
+    base_models_error_message = 'We were not able to make predictions using the following model(s): '
+
+    for model in models:
+        response[model] = {}
+        error_messages = []
+        
+        if model.lower() == 'rlm':
+            predictor = RLMPredictior(kekule_smiles = working_df['kekule_smiles'].values, smiles=working_df[smi_column_name].values)
+            print(predictor)
+        else:
+            break
+
+        pred_df = predictor.get_predictions()
+        print(pred_df)
+
+        pred_df = working_df.join(pred_df)
+        print(pred_df)
+        pred_df.drop(['mols', 'kekule_smiles'], axis=1, inplace=True)
+
+        # columns not present in original df
+        diff_cols = pred_df.columns.difference(df.columns)
+        df_res = pred_df[diff_cols]
+        print(df_res)
+
+        #response_df = pd.merge(df, pred_df, how='inner', left_on=smi_column_name, right_on=smi_column_name)
+        # making sure the response df is of the exact same length (rows) as original df
+        response_df = pd.merge(df, df_res, left_index=True, right_index=True, how='inner')
+
+        return response_df
+
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
-
-# checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
-
-# read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
-
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
     
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -30,7 +64,15 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
     
 # run model
-outputs = my_model(smiles_list, ckpt)
+output_df = predict_df(smiles_list)
+
+print(output_df)
+
+sys.exit(0)
+
+OUTPUT_COLUMN_NAME = "xxxx"
+
+outputs = list(output_df[OUTPUT_COLUMN_NAME])
 
 # write output in a .csv file
 with open(output_file, "w") as f:
